0a8e38b89ff2acda5e6d227d0daff66d1625c9df
Suprotik007 <csuprotik@gmail.com>
2025-06-20 15:26:39 +0600

    merchant section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Components/Banner';
 import Services from '../Components/services/Services';
 import ClientLogosMarquee from '../Components/ClientLogosMarquee';
 import Benefits from '../Components/Benefits/Benefits';
+import BeMerchant from '../Components/BeMerchant';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
            <Services></Services>
            <ClientLogosMarquee></ClientLogosMarquee>
            <Benefits></Benefits>
+           <BeMerchant></BeMerchant>
         </div>
     );
 };
